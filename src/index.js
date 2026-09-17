1c5f86be077508e078f2e5abee5f3c79d0c84dce
Nate Dower <ndowe@illinois.edu>
2026-09-17 14:48:06 -0500

    Moved files around to attempt to deploy

--- src/index.js
+++ src/index.js
@@ -12,30 +12,3 @@ import './css/main.scss';
 // Scripts
 import './js/main.js';
 
-const navButtons = document.querySelectorAll('.navbar a.buttons');
-const sections = document.querySelectorAll('.top, .projects, .contact');
-
-function updateActiveNav() {
-  let currentSectionId = '';
-
-  sections.forEach((section) => {
-    // Get the distance of each section
-    const sectionTop = section.getBoundingClientRect().top;
-
-    if (sectionTop <= 150) {
-      currentSectionId = section.getAttribute('id');
-    }
-  });
-
-  // Sets button's active status
-  navButtons.forEach((button) => {
-    button.classList.remove('active');
-    if (button.getAttribute('href') === `#${currentSectionId}`) {
-      button.classList.add('active');
-    }
-  });
-}
-
-// Listeners
-window.addEventListener('scroll', updateActiveNav);
-document.addEventListener('DOMContentLoaded', updateActiveNav);
