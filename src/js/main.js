5c3bc737ccaf11c841b7c826ca313a2dbe8ab3ef
Nate Dower <ndowe@illinois.edu>
2026-09-21 09:30:31 -0500

    Image carousel finished

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,17 @@
 const navButtons = document.querySelectorAll('.navbar a.buttons');
 const sections = document.querySelectorAll('.top, .projects, .contact');
 const navBar = document.querySelector('.navbar');
+const name = document.querySelector('.navbar a.name');
+const right = document.querySelector('.next');
+const left = document.querySelector('.prev');
+
+const slides = document.querySelectorAll('.mySlides');
+const dots = document.querySelectorAll('.dot');
+
+let slideNum = 1;
+showSlides(slideNum);
+
+
 
 function scrollUpdate() {
   let currentSectionId = '';
@@ -24,19 +35,51 @@ function scrollUpdate() {
     }
   });
 
-if (window.scrollY > 80) {
-    navBar.style.height = "30px";
-    navButtons.forEach((button) => {
-        button.style.fontSize = "15px";
-    });
-} else {
-    navBar.style.height = "80px";
-    navButtons.forEach((button) => {
-        button.style.fontSize = "30px";
-    }); 
+    if (window.scrollY > 80) {
+        navBar.style.height = "30px";
+        navButtons.forEach((button) => {
+            button.style.fontSize = "15px";
+            button.style.padding = "5px";
+        });
+        name.style.fontSize = "20px";
+    } else {
+        navBar.style.height = "80px";
+        navButtons.forEach((button) => {
+            button.style.fontSize = "30px";
+            button.style.padding = "10px";
+        }); 
+        name.style.fontSize = "40px";
+    }
 }
+
+// Next/previous controls
+right.addEventListener('click', () => showSlides(slideNum + 1));
+left.addEventListener('click', () => showSlides(slideNum - 1));
+
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideNum = n);
+}
+
+function showSlides(n) {
+  let i;
+  if (n > slides.length) {n = 1}
+  if (n < 1) {n = slides.length}
+  slideNum = n;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideNum-1].style.display = "block";
+  dots[slideNum-1].className += " active";
 }
 
 // Listeners
 window.addEventListener('scroll', scrollUpdate);
-document.addEventListener('DOMContentLoaded', updateActiveNav);
+scrollUpdate();
+
+
+
